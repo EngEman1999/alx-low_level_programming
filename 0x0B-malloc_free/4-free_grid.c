af1f5dea6fcaebf8ba5ec81526f6efdf86c6c360
EngEman1999 <eman170249@gmail.com>
2023-09-05 08:43:08 -0700

    malloc_free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,16 +1,134 @@
 #include <stdio.h>
+		
 #include <stdlib.h>
+		
 #include "main.h"
+		
 /**
- * free_grid - free a 2-dimensional array created by alloc_grid
- * @grid: a pointer to the first column of the first row of the array
- * @height: the number of rows in the array
+		
+ * free_grid - frees 2d array
+		
+ * @grid: 2d grid
+		
+ * @height: height dimension of grid
+		
+ * Description: frees memory of grid
+		
+ * Return: nothing
+		
  *
- * Return: void
+		
  */
+		
 void free_grid(int **grid, int height)
+		
 {
-	while (height)
-		free(grid[--height]);
+		
+	int i;
+		
+
+		
+	for (i = 0; i < height; i++)
+		
+	{
+		
+		free(grid[i]);
+		
+	}
+		
 	free(grid);
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+100-argstostr.c
+
+
+#include "main.h"
+		
+#include <stdlib.h>
+		
+/**
+		
+ * argstostr - main entry
+		
+ * @ac: int input
+		
+ * @av: double pointer array
+		
+ * Return: 0
+		
+ */
+		
+char *argstostr(int ac, char **av)
+		
+{
+		
+	int i, n, r = 0, l = 0;
+		
+	char *str;
+		
+
+		
+	if (ac == 0 || av == NULL)
+		
+		return (NULL);
+		
+
+		
+	for (i = 0; i < ac; i++)
+		
+	{
+		
+		for (n = 0; av[i][n]; n++)
+		
+			l++;
+		
+	}
+		
+	l += ac;
+		
+
+		
+	str = malloc(sizeof(char) * l + 1);
+		
+	if (str == NULL)
+		
+		return (NULL);
+		
+	for (i = 0; i < ac; i++)
+		
+	{
+		
+	for (n = 0; av[i][n]; n++)
+		
+	{
+		
+		str[r] = av[i][n];
+		
+		r++;
+		
+	}
+		
+	if (str[r] == '\0')
+		
+	{
+		
+		str[r++] = '\n';
+		
+	}
+		
+	}
+		
+	return (str);
+		
 }
